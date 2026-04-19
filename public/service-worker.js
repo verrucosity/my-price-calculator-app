4891a9d63ceed2b492748ad2b492241b370d5d4a
Estevan Mejia <verrucosity@gmail.com>
2026-04-18 20:42:17 -0400

    Add service worker for true offline support

---
+++ public/service-worker.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'ncn-toolbox-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/static/css/main.a127339e.css',
+  '/static/js/main.50978081.js',
+  '/static/js/453.01457f59.chunk.js'
+];
+
+// Install service worker and cache app files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).catch((err) => {
+        console.log('Cache addAll error:', err);
+        // Continue even if some resources fail to cache
+        return Promise.resolve();
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate service worker and clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch strategy: cache first, fall back to network
+self.addEventListener('fetch', (event) => {
+  // Only handle GET requests
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached response if available
+      if (response) {
+        return response;
+      }
+
+      // Otherwise fetch from network
+      return fetch(event.request).then((response) => {
+        // Don't cache non-successful responses
+        if (!response || response.status !== 200) {
+          return response;
+        }
+
+        // Clone the response to cache it
+        const responseToCache = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      }).catch(() => {
+        // If network fails, try to return a cached version
+        return caches.match(event.request);
+      });
+    })
+  );
+});
--- src/index.js
+++ src/index.js
@@ -15,3 +15,12 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// Register service worker for offline support
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').catch((err) => {
+      console.log('Service Worker registration failed:', err);
+    });
+  });
+}
